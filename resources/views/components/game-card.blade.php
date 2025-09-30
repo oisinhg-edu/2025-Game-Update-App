d4501101113596279d74d435badc64b5f2cfa970
Oisin <N00242930@iadt.ie>
2025-09-30 16:49:47 +0100

    created components and layout for viewing all games.

--- resources/views/components/game-card.blade.php
+++ resources/views/components/game-card.blade.php
@@ -1,6 +1,6 @@
-@props(['title', 'description', 'image'])
+@props(['title', 'description', 'cover_img'])
 
 <div class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300">
     <h4 class="font-bold text-lg">{{ $title }}</h4>
-    <img src="{{ asset('images/games/' . $image) }}" alt="{{ $title }}">
+    <img src="{{ asset('images/games/' . $cover_img) }}" alt="{{ $title }}">
 </div>
