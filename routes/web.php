73415be8c8f12715093efe1c37a2ebeeb7123db1
Oisin <N00242930@iadt.ie>
2025-09-30 16:09:48 +0100

    Added all game routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/games', [GameController::class, 'index'])->name('games.index');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// routes for game CRUD
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
+Route::get('/games/create', [GameController::class, 'create'])->name('games.create');
+Route::get('/games/{game}', [GameController::class, 'show'])->name('games.show');
+Route::post('/games', [GameController::class, 'store'])->name('games.store');
+
+Route::get('/games/{game}/edit', [GameController::class, 'edit'])->name('games.edit');
+Route::put('/games/{game}', [GameController::class, 'update'])->name('games.update');
+Route::delete('/games/{game}', [GameController::class, 'destroy'])->name('games.destroy');
 
 require __DIR__.'/auth.php';
